884c4ed83bb146b1f013e82fd4b05916702f8997
Alcimarrfilho <alcimarrosalbenvindo27@gmail.com>
2026-04-25 23:39:50 -0300

    Troquei o script do jogador e apaguei o velho

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,34 +1,55 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const JUMP_VELOCITY = -500.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-# Puxa o desenho 
+var attacking = false
+var attack_cooldown = 0.0
+
 @onready var sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# 1. Aplica a Gravidade
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# 2. Faz o Pulo (Barra de Espaço)
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# 3. Descobre os botoes direita ou esquerda
 	var direction = Input.get_axis("ui_left", "ui_right")
 	
 	if direction:
 		velocity.x = direction * SPEED
-		# Vira o boneco para o lado certo
 		sprite.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	# Aplica o movimento na tela 
-	move_and_slide()
+	# Ataque
+	if Input.is_action_just_pressed("ui_select") and is_on_floor() and not attacking:
+		attacking = true
+		attack_cooldown = 0.6
 	
-	# Rastreador para ver posiçao:
-	print("Minha posição é: ", position)
+	if attacking:
+		if sprite.animation != "attack":
+			sprite.animation = "attack"
+			sprite.play()
+		attack_cooldown -= delta
+		if attack_cooldown <= 0:
+			attacking = false
+
+	move_and_slide()
+
+	# Controla as animações SÓ SE não estiver atacando
+	if not attacking:
+		if not is_on_floor():
+			if sprite.animation != "jump":
+				sprite.animation = "jump"
+				sprite.play()
+		elif direction != 0:
+			if sprite.animation != "run":
+				sprite.animation = "run"
+				sprite.play()
+		else:
+			if sprite.animation != "idle":
+				sprite.animation = "idle"
+				sprite.play()
