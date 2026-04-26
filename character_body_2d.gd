f112322159c4187d91c3f4697c97f857b38365a5
Alcimarrfilho <alcimarrosalbenvindo27@gmail.com>
2026-04-26 01:23:29 -0300

    limpando arquivos

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,6 +1,5 @@
-extends CharacterBody2D
-
-const SPEED = 300.0
+extends CharacterBody2
+const SPEED = 300.0  
 const JUMP_VELOCITY = -500.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
